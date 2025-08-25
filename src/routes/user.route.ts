f632700f9472584b532fe711b8700b1009170220
DilshanHMT <156901370+DilshanHMT@users.noreply.github.com>
2025-08-25 20:32:40 +0500

    Update user.route.ts

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -6,7 +6,7 @@ import UserController from '../controllers/user.controller';
 import AuthMiddleware from '../middlewares/auth.middleware';
 
 /*user routes*/
-router.post('/create', [celebrate(user.userValidation.addData)], [AuthMiddleware.authenticateToken], UserController.addData)
+router.post('/create', [celebrate(user.userValidation.addData)], UserController.addData)
 router.get('/get', [AuthMiddleware.authenticateToken], UserController.getData)
 
 export { router as userRouter };
